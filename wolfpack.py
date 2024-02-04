0c02d612f9a506461963a809e177b96e27957bb4
Cas Hagenaar <you@example.com>
2024-02-04 15:48:27 +0100

    Add variables to wolfpack.py

---
+++ wolfpack.py
@@ -0,0 +1,15 @@
+#Global variable for grid
+world_grid = [[]]
+#Global variable for grid size
+world_grid-size = 14
+#Global variable for number of ships
+ship_num = 3
+#Global variable for game over
+game_over = False
+#Global variable for number of ships sunk
+ship_destroyed = 0
+#Global variable for ship position
+ship_position = [[]]
+#Global variable for alphabet
+alphabet = ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
